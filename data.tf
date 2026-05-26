ce910da25eafa14fb1a50cbab2053446aa0bfdc9
harry.sarshogh <harry.sarshogh@gmail.com>
2026-05-26 22:34:09 +0100

    feat: implement tf-atom-kms-alias-aws module

--- data.tf
+++
@@ -1,8 +0,0 @@
-# Data sources for the module.
-#
-# Add data sources here when you need to reference existing
-# AWS resources (e.g., IAM policies, VPCs, KMS keys).
-#
-# Example:
-# data "aws_caller_identity" "current" {}
-# data "aws_region" "current" {}
